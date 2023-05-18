2771bc11c8182abb745273ba3d6400a01b4083db
Edgar Bermudez <edgar.bermudez@gmail.com>
2023-05-17 19:08:54 -0600

    Update setup.py

--- ML_in_the_enterprise/titanic/setup.py
+++ ML_in_the_enterprise/titanic/setup.py
@@ -1,1 +1,16 @@
-
+from setuptools import find_packages
+from setuptools import setup
+REQUIRED_PACKAGES = [
+    'gcsfs==0.7.1',
+    'dask[dataframe]==2021.2.0',
+    'google-cloud-bigquery-storage==1.0.0',
+    'six==1.15.0'
+]
+setup(
+    name='trainer',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(), # Automatically find packages within this directory or below.
+    include_package_data=True, # if packages include any data files, those will be packed together.
+    description='Classification training titanic survivors prediction model'
+)
