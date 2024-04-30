b93bea8561ecfeff62f20301020fe63d27403fe6
Edgar <edgar.bermudez@gmail.com>
2024-04-30 10:34:15 -0600

    feat: evaluation chain for RAG

--- developing_LLM_applications_with_langchain/my_llm_evaluation_example.py
+++ developing_LLM_applications_with_langchain/my_llm_evaluation_example.py
@@ -30,3 +30,34 @@ eval_result = evluator.evaluate_strings (
 	prediction = "That is a subjective statement and I can not answer that.")
 
 print(eval_result)
+
+
+###
+
+# Now using a RAG application and chains to do evaluation
+
+embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+docstorage = Chroma.from_documents(docs, embedding)
+llm = OpenAI(model_name="gpt-3.5-turbo-instruct", openai_api_key=openai_api_key)
+
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docstorage.as_retriever(), input_key="question")
+
+# Generate the model responses using the RetrievalQA chain and question_set
+predictions = qa.apply(question_set)
+
+# Define the evaluation chain
+eval_chain = QAEvalChain.from_llm(llm)
+
+# Evaluate the ground truth against the answers that are returned
+results = eval_chain.evaluate(question_set,
+                              predictions,
+                              question_key="question",
+                              prediction_key="result",
+                              answer_key='answer')
+
+for i, q in enumerate(question_set):
+    print(f"Question {i+1}: {q['question']}")
+    print(f"Expected Answer: {q['answer']}")
+    print(f"Model Prediction: {predictions[i]['result']}\n")
+    
+print(results)
